8d8e3979d0c92f56992ef78442cabb9ccf3c85e1
Tamah <eng.tammah@gmail.com>
2025-09-19 23:32:27 +0300

    ..

--- Backend/ElectronicsStore.Infrastructure/obj/Debug/net9.0/ElectronicsStore.Infrastructure.AssemblyInfo.cs
+++ Backend/ElectronicsStore.Infrastructure/obj/Debug/net9.0/ElectronicsStore.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicsStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd6779178f1373276406c61553bd23ea49170a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ba08cd96f0a62b1b2902007fcec65aadfd0b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicsStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicsStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
